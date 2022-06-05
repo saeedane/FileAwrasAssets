5432dbb7364d552fe652840585cec13616b45e4e
saeedane <dvdplay405@gmail.com>
2022-06-05 22:49:51 +0100

    edit file awras_script_news

--- js/awras_script_news.js
+++ js/awras_script_news.js
@@ -54,7 +54,7 @@ var swiper = new Swiper("#newsTopSlider", { autoplay: { delay: 5e3 }, navigation
     slidesPerGroup: 2,
     loop: !1,
     loopFillGroupWithBlank: !0,
-    pagination: { el: ".swiper-pagination", clickable: true },
+    pagination: { el: ".swiper-pagination", clickable: true},
     breakpoints: { 320: { slidesPerView: 2, spaceBetween: 10 }, 575: { slidesPerView: 2, spaceBetween: 10 }, 768: { slidesPerView: 2 }, 991: { slidesPerView: 3 }, 1200: { slidesPerView: 4 } },
 })),
     (swiper = new Swiper("#IllusStorSlider", {
